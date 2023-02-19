4f580072e57a56fcd1b1595cbf767ff4cff42c37
AaronManuel15 <aaronmanuel15@gmail.com>
2023-02-18 23:58:44 -0600

    I dont know at this point

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -7,24 +7,19 @@
  *
  * @array: The array to be looked at
  * @size: Number of elements in @array
- * Return: digit length
+ * Return: max value in array
  */
 size_t maxValueLen(int *array, size_t size)
 {
-	int max = array[0], len = 1;
-	size_t i = 0, count = 1;
+	int max = array[0];
+	size_t i = 0;
 
 	for (i = i; i < size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
 	}
-	while ((max / len) > 0)
-	{
-		len *= 10;
-		count += 1;
-	}
-	return (--count);
+	return (max);
 }
 /**
  * countingSort - sorts an array of ints in ascending order by place
@@ -69,9 +64,10 @@ void radix_sort(int *array, size_t size)
 {
 	if (array && size > 1)
 	{
-		size_t place, digitCount = maxValueLen(array, size), i;
+		size_t place;
+		int max = maxValueLen(array, size);
 
-		for (i = 1, place = 1; i <= digitCount; i++, place *= 10)
+		for (place = 1; max / place > 0; place *= 10)
 		{
 			countingSort(array, size, place);
 			print_array(array, size);
